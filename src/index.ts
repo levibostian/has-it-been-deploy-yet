155de6b79e5143e97d2d8dcd06035e466fc29888
Levi Bostian <levi.bostian@gmail.com>
2023-11-20 07:34:25 -0600

    edit api a little

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { PackageManager, getPackageManager } from "./packageManagers";
 import { runCheckIsItDeployed } from "./runner";
 
-export const isItDeployed = (args: { packageManager: 'npm' | 'cocoapods' | 'maven', packageName: string, packageVersion: string }, maxRetries: number | undefined = undefined) => {  
+export const isItDeployed = (args: { packageManager: 'npm' | 'cocoapods' | 'maven', packageName: string, packageVersion: string }, opts?: { maxRetries: number | undefined }) => {  
   const packageManager: PackageManager | undefined = getPackageManager(args.packageManager)
 
   if (!packageManager) throw new Error('Package manager not supported');
 
-  // run retries for 1 minute 
-  return runCheckIsItDeployed({ packageManager, packageName: args.packageName, packageVersion: args.packageVersion }, { maxRetries, secondsBetweenRetries: 5 })
+  return runCheckIsItDeployed({ packageManager, packageName: args.packageName, packageVersion: args.packageVersion }, { maxRetries: opts?.maxRetries, secondsBetweenRetries: 5 })
 }
